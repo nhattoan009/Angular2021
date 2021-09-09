d8492c3fe930316cd3f68735df71dd24c29916a7
Toanhn <80703338+nhattoan009@users.noreply.github.com>
2021-09-09 21:53:01 +0700

    cascadinh-comboBox-Loc tp theo tinh thanh-basic

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -25,15 +25,56 @@ export class HeroComponent implements OnInit {
       discount: false
     }
   ]
+  public tp: string[] = ['thanh pho'];
+  public tinh = [
+    {
+      tinh: 'tinh thanh'
+      
+    },
+    {
+      tinh: 'An Giang',
+      tp: [
+        'tp Long Xuyen',
+        'tp Chau Doc',
+        'tp An Phu',
+        'tp Chau Thanh',
+        'tp Tinh Bien'
+      ]
+    },
+    {
+      tinh: 'Kien Giang',
+      tp: [
+        'tp Ha Tien',
+        'tp Rach Gia',
+        'tp Phu Quoc'
+      ]
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('trai cay ', this.traiCay);
+    console.log('City ', this.tinh);
   }
   public reset(): void {
     this.name = '';
     console.log('click next');
+  }
+
+  public changeCity(event: any) {
+    const city = event.target.value;
+    if (!city) { return; }
+
+    //cach 1
+    // console.log('event', city);
+    // const search = this.tinh.filter((data) => data.tinh === city);
+    // console.log('Search ', search);
+    // if (search && search.length > 0) {
+    //   this.tp = search[0].tp;
+    // }
+
+    //cach 2
+    this.tp = this.tinh.find(data => data.tinh === city) ?.tp || [];
   }
 
 }
