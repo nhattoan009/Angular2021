8b5d99eb1dd9d8efe7c8bae0280d4b4bc4de8fed
Toanhn <80703338+nhattoan009@users.noreply.github.com>
2021-09-09 16:14:45 +0700

    ngFor-object-basic

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -8,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class HeroComponent implements OnInit {
   public name = 'Toan';
   public age = 20;
+  public traiCay = [
+    {
+      name: 'cam',
+      price: 2000,
+      discount: true
+    },
+    {
+      name: 'chuoi',
+      price: 2500,
+      discount: true
+    },
+    {
+      name: 'nho',
+      price: 0,
+      discount: false
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('trai cay ', this.traiCay);
   }
-  public reset(): void{
+  public reset(): void {
     this.name = '';
     console.log('click next');
   }
